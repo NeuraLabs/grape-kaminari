6b173104e6f065e2a09841fc64fa8be8dd8e5a7a
nikitasherman <nikita.sherman@icloud.com>
2015-03-12 11:37:45 +0200

    change grape parameter names in grape kaminari

--- lib/grape-kaminari.rb
+++ lib/grape-kaminari.rb
@@ -2,10 +2,12 @@ module Grape
   module Kaminari
     class << self
       def post_0_9_0_grape?
-        Gem::Version.new(Grape::VERSION) > Gem::Version.new('0.9.0')
+        if Grape::VERSION
+          Gem::Version.new(Grape::VERSION) > Gem::Version.new('0.9.0')
+        end
       end
     end
   end
 end
 
-require "grape/kaminari"
+require 'grape/kaminari'
--- lib/grape/kaminari/max_value_validator.rb
+++ lib/grape/kaminari/max_value_validator.rb
@@ -1,4 +1,5 @@
 module Grape
+  
   module Kaminari
     base = if post_0_9_0_grape?
              Grape::Validations::Base
@@ -13,12 +14,18 @@ module Grape
         attr = params[attr_name]
         if attr && @option && attr > @option
           if Gem::Version.new(Grape::VERSION) >= Gem::Version.new('0.9.0')
-            raise Grape::Exceptions::Validation, params: [@scope.full_name(attr_name)], message: "must be less than #{@option}"
+            raise Grape::Exceptions::Validation, 
+              params: [@scope.full_name(attr_name)], 
+              message: "must be less than #{@option}"
           else
-            raise Grape::Exceptions::Validation, param: @scope.full_name(attr_name), message: "must be less than #{@option}"
+            raise Grape::Exceptions::Validation, 
+              param: @scope.full_name(attr_name), 
+              message: "must be less than #{@option}"
           end
         end
       end
     end
+  
   end
+
 end
