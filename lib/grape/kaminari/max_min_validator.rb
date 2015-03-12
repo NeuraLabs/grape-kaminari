0079e5337e123c75175fe6231804b93cb9fc8355
nikitasherman <nikita.sherman@icloud.com>
2015-03-12 15:51:03 +0200

    update validators

---
+++ lib/grape/kaminari/max_min_validator.rb
@@ -0,0 +1,53 @@
+module Grape
+  module Kaminari
+    base = if post_0_9_0_grape?
+             Grape::Validations::Base
+           else
+             Grape::Validations::SingleOptionValidator
+           end
+
+    class MaxValueValidator < base
+      
+      def validate_param!(attr_name, params)
+        return unless params[attr_name]
+
+        attr = params[attr_name]
+        if attr && @option && attr > @option
+          if Gem::Version.new(Grape::VERSION) >= Gem::Version.new('0.9.0')
+            raise Grape::Exceptions::Validation, 
+              params: [@scope.full_name(attr_name)], 
+              message: "must be less than #{@option}"
+          else
+            raise Grape::Exceptions::Validation, 
+              param: @scope.full_name(attr_name), 
+              message: "must be less than #{@option}"
+          end
+        end
+      end
+
+    end
+
+    class MinValueValidator < base
+      
+      def validate_param!(attr_name, params)
+        return unless params[attr_name]
+
+        attr = params[attr_name]
+        if attr && @option && attr < @option
+          if Gem::Version.new(Grape::VERSION) >= Gem::Version.new('0.9.0')
+            raise Grape::Exceptions::Validation, 
+              params: [@scope.full_name(attr_name)], 
+              message: "must be less than #{@option}"
+          else
+            raise Grape::Exceptions::Validation, 
+              param: @scope.full_name(attr_name), 
+              message: "must be less than #{@option}"
+          end
+        end
+      end
+      
+    end
+
+  end
+
+end
